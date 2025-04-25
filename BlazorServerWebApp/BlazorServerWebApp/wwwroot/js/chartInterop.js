a9d42d9e8d35943824eeae75bbb59c6b65e9b543
jslee <jslee@plaif.com>
2025-04-25 12:08:27 +0900

    싸인파 그리는 것까지 테스트

---
+++ BlazorServerWebApp/BlazorServerWebApp/wwwroot/js/chartInterop.js
@@ -0,0 +1,45 @@
+﻿// wwwroot/js/chartInterop.js
+
+let uplot;
+export const chartInterop = {
+    initChart: function () {
+        const opts = {
+            title: "실시간 로봇 데이터",
+            width: 800,
+            height: 400,
+            series: [
+                {},
+                {
+                    label: "값",
+                    stroke: "blue"
+                }
+            ],
+            axes: [
+                {},
+                {
+                    values: (u, vals) => vals.map(v => v.toFixed(2)),
+                }
+            ]
+        };
+
+        const data = [
+            [], // x
+            []  // y
+        ];
+
+        uplot = new uPlot(opts, data, document.getElementById("chart"));
+    },
+    addPoint: function (x, y) {
+        if (!uplot) return;
+
+        uplot.data[0].push(x);
+        uplot.data[1].push(y);
+
+        if (uplot.data[0].length > 500) {
+            uplot.data[0].shift();
+            uplot.data[1].shift();
+        }
+
+        uplot.setData(uplot.data);
+    }
+};
