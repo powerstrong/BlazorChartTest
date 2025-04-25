f4bc7789760a4cbc256112a6fd8b702a3ab109b6
jslee <jslee@plaif.com>
2025-04-25 14:54:19 +0900

    apply style

--- BlazorServerWebApp/BlazorServerWebApp/wwwroot/js/chartInterop.js
+++ BlazorServerWebApp/BlazorServerWebApp/wwwroot/js/chartInterop.js
@@ -1,17 +1,19 @@
-﻿// wwwroot/js/chartInterop.js
+﻿let chartMap = {};
+let count = 0;
+let limit = 3000;
 
-let uplot;
 export const chartInterop = {
-    initChart: function () {
+    initChart: function (id, strokeColor="red") {
         const opts = {
-            title: "실시간 로봇 데이터",
-            width: 800,
-            height: 400,
+            title: id,
+            width: 600,
+            height: 300,
             series: [
                 {},
                 {
                     label: "값",
-                    stroke: "blue"
+                    stroke: strokeColor,
+                    points: { show: false },
                 }
             ],
             axes: [
@@ -27,19 +29,30 @@ export const chartInterop = {
             []  // y
         ];
 
-        uplot = new uPlot(opts, data, document.getElementById("chart"));
+        const target = document.getElementById(id);
+        if (!target) return;
+
+        chartMap[id] = {
+            uplot: new uPlot(opts, data, target),
+            data: data
+        };
     },
-    addPoint: function (x, y) {
-        if (!uplot) return;
 
-        uplot.data[0].push(x);
-        uplot.data[1].push(y);
+    addPoint: function (id, x, y) {
+        const entry = chartMap[id];
+        if (!entry) return;
 
-        if (uplot.data[0].length > 500) {
-            uplot.data[0].shift();
-            uplot.data[1].shift();
+        entry.data[0].push(x);
+        entry.data[1].push(y);
+
+        if (count < limit) {
+            count++;
+        }
+        else {
+            entry.data[0].shift();
+            entry.data[1].shift();
         }
 
-        uplot.setData(uplot.data);
+        entry.uplot.setData(entry.data);
     }
 };
